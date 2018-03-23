c41f67a70f5b3983cf9ccc396d135ad762993512
root <tomtom9461@gmail.com>
2018-03-22 21:33:39 -0400

    first commit

---
+++ MVVMCustomPresentation/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using MVVMCustomPresentation.ViewModels;
+
+namespace MVVMCustomPresentation
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new CustomerViewModel();
+        }
+    }
+}
